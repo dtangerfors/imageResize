aa94fe626959832d94eedbf196493c7088c212e6
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2023-06-05 11:29:49 +0200

    Added script to create destination folder

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const setupQuestions = require('./lib/setup').questions;
 const collectImages = require('./lib/collectImages');
 const resize = require('./lib/imageResize');
+const createFolder = require('./lib/createOutputFolder');
 
 let settings = new Object, images = new Array;
 
 const main = async () => {
    await setupQuestions().then(result => settings = result);
    await collectImages().then(result => images = result);
-   await resize(settings, images).then(console.log('\n\nResizing… Check the output folder.\n'))
+   createFolder(settings);
+   await resize(settings, images).then(console.log('\n\nSkalar om… Kolla destinationsmappen.\n'))
 }
 
 main();
